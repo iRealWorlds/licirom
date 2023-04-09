c1c73a8d633294ddb5efed994f20b4065f3087de
Marin Calin <marincalin2551@hotmail.com>
2023-04-09 14:24:36 +0300

    Updated the theme colour.

--- WebApp/tailwind.config.js
+++ WebApp/tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
   ],
   important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#bddffc',
+          100: '#94ccfa',
+          200: '#6bb8f8',
+          300: '#4da9f7',
+          400: '#359af5',
+          500: '#318ce7',
+          600: '#2b7ad4',
+          700: '#2769c2',
+          800: '#1f4ba2',
+          900: '#1e428b',
+        }
+      }
+    },
   },
   plugins: [],
 }
