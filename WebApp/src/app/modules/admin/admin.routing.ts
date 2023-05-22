240ee7cad4bd66a6a6328e704830703f19bc321b
Marin Calin <marincalin2551@hotmail.com>
2023-05-22 22:28:54 +0300

    Added missing comma.

--- WebApp/src/app/modules/admin/admin.routing.ts
+++ WebApp/src/app/modules/admin/admin.routing.ts
@@ -5,7 +5,7 @@ export const adminRouting: Route[] = [
   {
       path: 'categories',
       loadChildren: () => import('src/app/modules/admin/auction-categories/auction-categories.module').then(m=>m.AuctionCategoriesModule)
-  }
+  },
   {
     path: 'auctions',
     loadChildren: () => import('src/app/modules/admin/pending-auctions/pending-auctions.module').then(m => m.PendingAuctionsModule)
