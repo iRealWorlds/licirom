d8281b7480f9c527b056f77f2ad6d4fb61fe9f51
Slice29 <slice29@DESKTOP-TS5JMBH>
2023-05-23 01:02:05 +0300

    Updated Auction Cards UI

--- WebApp/src/app/modules/auctions/auctions-list/auction-card/auction-card.component.ts
+++ WebApp/src/app/modules/auctions/auctions-list/auction-card/auction-card.component.ts
@@ -5,7 +5,9 @@ import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { ExpandablePipeModule } from '@licirom/modules/shared/expandable-pipe/expandable-pipe.module';
 import { User } from '@licirom/modules/users/user.model';
+import { DatePipe } from '@angular/common';
 import { AuctionStatus } from '@licirom/modules/auctions/auction-status.enum';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-auction-card',
@@ -26,4 +28,35 @@ export class AuctionCardComponent {
   displayCreatorName(user: User): string {
     return `${user.firstName} ${user.lastName}`;
   }
+
+  isAvailable(startDateString: string, endDateString: string) {
+    const date = new Date();
+    const startDate = new Date(startDateString);
+    const endDate = new Date(endDateString);
+
+    if (startDate < date && endDate > date) {
+      return true;
+    }
+
+    return false;
+  }
+  isinFuture(startDateString: string) {
+    const date = new Date();
+    const startDate = new Date(startDateString);
+
+    if (startDate > date) {
+      return true;
+    }
+
+    return false;
+  }
+
+  wasBefore(endDateString: string) {
+
+    const date = new Date();
+    const endDate = new Date(endDateString);
+    if (endDate < date) return true;
+    return false;
+  }
+
 }
